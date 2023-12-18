92d0f4d96fd819ba468f4e39afd6a1cbd1ce0931
koustavmoitra <koustavmoitra711@gmail.com>
2023-12-18 22:39:11 +0530

    initial commit

---
+++ App/Pages/Login.js
@@ -0,0 +1,34 @@
+import { View, Text, Image, StyleSheet } from 'react-native'
+import React from 'react'
+import Colors from '../Shared/Colors';
+import { FontAwesome } from '@expo/vector-icons';
+
+export default function Login() {
+  return (
+    <View>
+       <Text  style={{fontSize:65,textAlign:'center',marginTop:50}}>Khel⚽</Text>
+        <Text  style={{fontSize:25,textAlign:'center',marginTop:50,marginBottom:25}}>Ready, Steady, Play !</Text>
+        <Image source={require('./../Assets/Images/logo.jpg')} style={{width:'100%', height:350, alignItems:'center',resizeMode:'contain',borderRadius:50, borderColor:'#fff'}} />
+        <Text style={{fontSize:35,textAlign:'center',marginTop:50,fontStyle:'italic'}}> New to khelo App </Text>
+
+        <View style={styles.button}>
+        <FontAwesome style={{marginRight:15}} name="google" size={24} color="black" />
+            <Text>Sign in with Google</Text>
+        </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+     button : {
+        backgroundColor:Colors.primary,
+        padding:20,
+        margin:30,
+        borderRadius:50,
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'center',
+        marginTop:40
+
+     }
+    });
---
+++ App/Shared/Colors.js
@@ -0,0 +1,3 @@
+export default{
+    primary:'#6EC10A'
+}
